7a002e98b88295fd859e02ed048663211023124e
piyush <piyushjain543210@gmail.com>
2023-02-01 15:24:45 +0530

    coderefactor

---
+++ newstartyears/src/main/java/com/example/newstartyears/NewstartyearsApplication.java
@@ -0,0 +1,13 @@
+package com.example.newstartyears;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewstartyearsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewstartyearsApplication.class, args);
+	}
+
+}
---
+++ newstartyears/src/test/java/com/example/newstartyears/NewstartyearsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newstartyears;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewstartyearsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
